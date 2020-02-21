1b6c41a1f0904f3e6d0faafd4706c0b0cb67b8f4
Yanga Lusiti <yangalsiti@gmail.com>
2020-02-21 12:40:25 +0200

    Add files via upload

---
+++ Person.py
@@ -0,0 +1,16 @@
+class Person:
+    def __init__(self,name,age,gender,interests):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.interests = interests 
+        
+    def hello(self):
+        interests =' ' .join(self.interests)
+        return  'Hello, my name is {}  and I am {} years old. My interests are {}.'.format(self.name,self.age,interests) 
+    
+    
+person = Person('Ryan',30,'male',['being a hardarse','agile', 'ssd hard drives'] ) 
+greeting = person.hello()
+
+print(greeting)
